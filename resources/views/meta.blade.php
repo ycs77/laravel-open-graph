4e85a13ea664dfa9909d10fa3c079d73848d2309
ezitisitis <mail@ezitisitis.com>
2022-11-03 13:23:41 +0700

    Extend blade file

--- resources/views/meta.blade.php
+++ resources/views/meta.blade.php
@@ -11,6 +11,17 @@
     @if (OpenGraph::getImage())
         <meta property="og:image" content="{{ OpenGraph::getImage() }}" />
     @endif
+    @if(OpenGraph::getSiteName())
+        <meta property="og:site_name" content="{{ OpenGraph::getSiteName() }}" />
+    @endif
+    @if(OpenGraph::getLocale())
+        <meta property="og:locale" content="{{ OpenGraph::getLocale() }}" />
+    @endif
+    @if(OpenGraph::getAlternateLocale())
+        @foreach(OpenGraph::getAlternateLocale() as $alternateLocale)
+            <meta property="og:locale:alternate" content="{{ $alternateLocale }}" />
+        @endforeach
+    @endif
     @foreach (OpenGraph::getData() as $key => $value)
         <meta property="og:{{ $key }}" content="{{ $value }}" />
     @endforeach
